74c6389e212ab5bd4b46338359617a3e7e01d556
Alexandre barbier <alexandr.barbier@gmail.com>
2016-03-09 14:57:51 +0100

    implement today widget improve code

--- SmokeStopper/SmokeStopper/Model/smokeManager.swift
+++ SmokeStopper/SmokeStopper/Model/smokeManager.swift
@@ -9,104 +9,138 @@
 import UIKit
 
 let SmokeManagerSharedInstance = smokeManager()
+let SmokeManagerTodaySharedInstance = smokeManager(today:true)
+let userDefault = NSUserDefaults(suiteName: "group.SmokeStopper")
 
 class smokeCount : NSObject, NSCoding {
     var date : NSDate = NSDate()
     var count : Int = 0
+    private struct coderKey {
+        static let count = "count"
+        static let date = "date"
+    }
     
     required init?(coder aDecoder: NSCoder) {
         super.init()
-        count = aDecoder.decodeIntegerForKey("count")
-        date = aDecoder.decodeObjectForKey("date") as! NSDate
+        count = aDecoder.decodeIntegerForKey(coderKey.count)
+        date = aDecoder.decodeObjectForKey(coderKey.date) as! NSDate
     }
     
-    override init() {
-        
+    init(count:Int,date:NSDate) {
+        super.init()
+        self.count = count
+        self.date = date
     }
     
     func encodeWithCoder(aCoder: NSCoder) {
-        aCoder.encodeObject(date, forKey: "date")
-        aCoder.encodeInteger(count, forKey: "count")
+        aCoder.encodeObject(date, forKey: coderKey.date)
+        aCoder.encodeInteger(count, forKey: coderKey.count)
     }
 }
 
 class smokeManager: NSObject {
     
-    struct coderKey {
+    struct userDefaultKey {
         static let historyKey = "smokeHistory"
         static let lastSmokeDate = "lastSmokeDate"
         static let maxSmokePerDay = "maxSmokePerDay"
         static let intervalMin = "intervalMin"
         static let intervalHour = "intervalHour"
+        static let todayCount = "todayCount"
     }
     
     struct lastSmokeInfo {
-
         var date = NSDate() {
             didSet {
-                userDefault!.setObject(date, forKey: coderKey.lastSmokeDate)
+                smokeManager.save(date, key: userDefaultKey.lastSmokeDate)
+            }
+        }
+        
+        var todayCount: Int {
+            get {
+                return userDefault!.integerForKey(userDefaultKey.todayCount)
+            }
+            set {
+                smokeManager.saveInteger(newValue, key: userDefaultKey.todayCount)
             }
         }
         
         var maxSmokePerDay : Int {
             get {
-                return userDefault!.integerForKey(coderKey.maxSmokePerDay)
+                return userDefault!.integerForKey(userDefaultKey.maxSmokePerDay)
             }
             set {
-                userDefault!.setInteger(newValue, forKey: coderKey.maxSmokePerDay)
+                smokeManager.saveInteger(newValue, key: userDefaultKey.maxSmokePerDay)
             }
         }
         
         var smokeInterval : (hour:Int, min:Int) {
             get {
-                let min = userDefault!.integerForKey(coderKey.intervalMin)
-                let hour = userDefault!.integerForKey(coderKey.intervalHour)
+                let min = userDefault!.integerForKey(userDefaultKey.intervalMin)
+                let hour = userDefault!.integerForKey(userDefaultKey.intervalHour)
                 return (hour,min)
             }
             set {
-                userDefault!.setInteger(newValue.min, forKey: coderKey.intervalMin)
-                userDefault!.setInteger(newValue.hour, forKey: coderKey.intervalHour)
+                smokeManager.saveInteger(newValue.min, key: userDefaultKey.intervalMin)
+                smokeManager.saveInteger(newValue.hour, key: userDefaultKey.intervalHour)
             }
         }
     }
     
     var history = [smokeCount]()
     var lastSmoke = lastSmokeInfo()
-    static let userDefault = NSUserDefaults(suiteName: "group.smokeStopper")
     
-    override init() {
+    private init(today:Bool) {
+        let coder = userDefault!
+        if let lSdate = coder.objectForKey(userDefaultKey.lastSmokeDate) as? NSDate {
+            lastSmoke.date =  lSdate
+        }
+    }
+    
+    private override init() {
         super.init()
-        let coder = smokeManager.userDefault!
+        let coder = userDefault!
         
-        if let lSdate = coder.objectForKey(coderKey.lastSmokeDate) as? NSDate {
+        if let lSdate = coder.objectForKey(userDefaultKey.lastSmokeDate) as? NSDate {
             lastSmoke.date =  lSdate
         }
         
-        if let histo = coder.objectForKey(coderKey.historyKey) {
+        if let histo = coder.objectForKey(userDefaultKey.historyKey) {
             let k = histo as! NSMutableArray
             for val in k {
                 let sCount = NSKeyedUnarchiver.unarchiveObjectWithData(val as! NSData) as? smokeCount
                 history.append(sCount!)
             }
         }
+        if let _ = history.last {
+            history.last!.count = lastSmoke.todayCount
+        }
     }
     
     func saveDay(todayCount: Int) {
-        let sCount = smokeCount()
-        sCount.date = NSDate()
-        sCount.count = todayCount
+        let sCount = smokeCount(count: todayCount, date: NSDate())
         let calendar = NSCalendar.currentCalendar()
-        
+        lastSmoke.todayCount = todayCount
+    
         if let last = history.last where calendar.compareDate(NSDate(), toDate: last.date, toUnitGranularity: NSCalendarUnit.Day) == NSComparisonResult.OrderedSame {
             history.removeLast()
         }
-        
+
         history.append(sCount)
-        let coder = smokeManager.userDefault!
         let k = NSMutableArray()
         for val in history {
             k.addObject(NSKeyedArchiver.archivedDataWithRootObject(val))
         }
-        coder.setObject(k, forKey: coderKey.historyKey)
+        smokeManager.save(k, key: userDefaultKey.historyKey)
+    }
+    
+    private class func save(object:AnyObject, key:String) {
+        userDefault!.setObject(object, forKey: key)
+        userDefault!.synchronize()
+    }
+    
+    private class func saveInteger(object:Int, key:String) {
+        userDefault!.setInteger(object, forKey: key)
+        userDefault!.synchronize()
     }
 }
--- SmokeStopper/today/TodayViewController.swift
+++ SmokeStopper/today/TodayViewController.swift
@@ -12,31 +12,27 @@ import NotificationCenter
 class TodayViewController: UIViewController, NCWidgetProviding {
         
     @IBOutlet weak var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let sManager = smokeManager()
-       titleLabel.text = "alex \(sManager.history)"
+
+      
         // Do any additional setup after loading the view from its nib.
     }
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
-        
-    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)) {
-        
-
-        // Perform any setup necessary in order to update the view.
-
-        // If an error is encountered, use NCUpdateResult.Failed
-        // If there's no update required, use NCUpdateResult.NoData
-        // If there's an update, use NCUpdateResult.NewData
+        titleLabel.text = "today count : \(SmokeManagerTodaySharedInstance.lastSmoke.todayCount)"
 
         completionHandler(NCUpdateResult.NewData)
     }
     
+    @IBAction func addTouch(sender: AnyObject) {
+        SmokeManagerTodaySharedInstance.lastSmoke.date = NSDate()
+        SmokeManagerTodaySharedInstance.lastSmoke.todayCount += 1
+    }
 }
